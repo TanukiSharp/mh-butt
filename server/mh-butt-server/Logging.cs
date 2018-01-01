c19fb3414e7a0c7657bc5eada1be3c037e68d5b8
Sebastien ROBERT <sebastien.saigo@gmail.com>
2018-01-01 22:43:14 +0900

    First commit, scaffolding of the client and server parts

---
+++ server/mh-butt-server/Logging.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace mh_butt_server
+{
+    public class ConsoleExLoggerProvider : ILoggerProvider
+    {
+        private readonly LogLevel logLevel;
+
+        public ConsoleExLoggerProvider(IConfiguration config)
+        {
+            logLevel = config.GetValue<LogLevel>("Logging:ConsoleEx:LogLevel:Default");
+            Console.WriteLine(logLevel);
+        }
+
+        private class ConsoleExLogger : ILogger
+        {
+            private static readonly object syncRoot = new object();
+            private readonly string categoryName;
+            private readonly LogLevel logLevel;
+
+            public ConsoleExLogger(string categoryName, LogLevel logLevel)
+            {
+                this.categoryName = categoryName;
+                this.logLevel = logLevel;
+            }
+
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return (int)logLevel >= (int)this.logLevel;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                if (IsEnabled(logLevel) == false)
+                    return;
+
+                lock (syncRoot)
+                {
+                    Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss.fff")} | {logLevel} | {categoryName} | {formatter(state, exception)}");
+                    Console.Out.Flush();
+                }
+            }
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new ConsoleExLogger(categoryName, logLevel);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
